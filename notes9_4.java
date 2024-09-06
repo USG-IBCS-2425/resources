d3e3c7d67afd0505bf7290cb9adf0ede3da1fb05
Zack Considine <zack_considine@M016196.aw.org>
2024-09-06 14:34:37 -0700

    notes from day1

---
+++ notes9_4.java
@@ -0,0 +1,39 @@
+// These are comments
+// All code is inside the braces for our class
+
+
+class notes9_4 {
+	// public is required so that this method
+	// can be accessed outside of the class
+	// static means that the method stays the same
+	// for all instances of the class
+	// void means no output
+	// String[] args is the parameters
+	public static void main(String[] args) {
+		
+		// print stuff
+		System.out.println("Hello World!");
+		System.out.println("We love " + "CS");
+		int x = 5;
+		byte b = -10;
+		long l = 9833475456456758L;
+		double dub = 33.3345345643859347539;
+		boolean t = true;
+		char c = 'z';
+		double y = 10.5;
+		y = y + 2;
+
+		String name = "Mr. Considine";
+
+		System.out.println(x);
+		System.out.println(b);
+		System.out.println(l);
+		System.out.println(dub);
+		System.out.println(t);
+		System.out.println(c);
+
+
+	}
+
+
+}
