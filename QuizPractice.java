046cacd6bde57fd2b3eb95b6507ebd78aac0eaad
Zack Considine <zack_considine@m016439.aw.org>
2024-09-30 12:40:08 -0700

    quiz practice

---
+++ QuizPractice.java
@@ -0,0 +1,24 @@
+// Q1: Find the average value of an array
+// of doubles
+public static double avgDoub(double[] d) {
+
+}
+
+// Q2: Find the longest word in an array
+// of strings
+public static String longestWord(String[] s) {
+	
+}
+
+// Q3: Determine the most common character
+// in an array of strings
+public static Character mostCommon(String[] s) {
+	
+}
+
+
+class QuizPractice {
+	public static void main(String[] args) {
+		
+	}
+}
